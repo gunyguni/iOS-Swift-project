93c1bc050c071c8e697382220a1b18fad187e389
John Park <manpkh95@gmail.com>
2022-03-13 01:31:47 +0900

    Section 3 COMPLETED

---
+++ I-Am-Poor/I-Am-Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Poor
+//
+//  Created by 건희 on 2022/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
