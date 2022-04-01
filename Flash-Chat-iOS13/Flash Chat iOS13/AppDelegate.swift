b45010206e3d14775d873825b4ce93240e58cb01
John Park <manpkh95@gmail.com>
2022-04-01 22:29:05 +0900

    Section 15 DONE

--- Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
+        
         return true
     }
 
---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Meesage.swift
+//  Flash Chat iOS13
+//
+//  Created by Keon Hee Park on 2022/04/01.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String 
+}
