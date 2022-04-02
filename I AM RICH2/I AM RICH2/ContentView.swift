fbd3d0fa8fb6619d555314e8ff18220c7e168219
John Park <manpkh95@gmail.com>
2022-04-02 14:29:58 +0900

    SwiftUI Practice 1 DONE

---
+++ I AM RICH2/I AM RICH2/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I AM RICH2
+//
+//  Created by Keon Hee Park on 2022/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0, alignment: .center )
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+    }
+}
---
+++ I AM RICH2/I AM RICH2/I_AM_RICH2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_AM_RICH2App.swift
+//  I AM RICH2
+//
+//  Created by Keon Hee Park on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct I_AM_RICH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
